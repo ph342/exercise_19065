057f5c6e721ef92813d6b25629cc100444875431
Dominik <44980652+ph342@users.noreply.github.com>
2019-06-07 15:00:41 +0100

    First commit

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -6,3 +6,10 @@ it('when the system date and the date to format are the same day formats as "TOD
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
   expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
 });
+
+it('when the system date and the date to format are not the same day formats as "DD/MM/YYYY"', () => {
+  const December = 11; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2017, December, 15, 10, 5).getTime(); //Different day same time
+  expect(format(dateTimeToFormat, systemDateTime)).toBe('15/12/2017');
+});
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,14 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  
+  var systemDateTime = new Date();
+  var dateToFormatTime = new Date();
+  systemDateTime.setTime(systemDateTimeMillis);
+  dateToFormatTime.setTime(dateToFormatTimeMillis);
+  
+  var dateDiff = Math.floor((Date.UTC(systemDateTime.getFullYear(), systemDateTime.getMonth(), systemDateTime.getDate()) - Date.UTC(dateToFormatTime.getFullYear(), dateToFormatTime.getMonth(), dateToFormatTime.getDate()) ) /(1000 * 60 * 60 * 24));
+  
+  if (dateDiff == 0)
+    return 'TODAY';
+  else
+    return dateToFormatTime.getDate() + '/' + eval(dateToFormatTime.getMonth()+1) + '/' + dateToFormatTime.getFullYear();
 };
